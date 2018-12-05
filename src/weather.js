7436edc663e7ee347f13be4f1682df94a0280ab5
Wojciech Opulski <wojtekopulski@gmail.com>
2018-12-05 17:55:35 +0100

    first fetches / constructor structure

--- src/weather.js
+++ src/weather.js
@@ -1,14 +1,36 @@
+import { get } from "https";
+
 class Weather {
     constructor() {
         console.log('Weather loaded')
+
         // TODO: na wzór tego utworzyć atrybuty które moga się przydać, w zalezności od tego co jest w API ofc
-        this.temp = 20;
-        this.condition = 'wietrzenie';
+        this.temp;      // temperatura, do wyboru jednostki Kelvin(domoślna), Celsiusza, Farenheita
+        this.condition; // grupa parametrow (Rain, Snow, Extreme)
+        this.windspeed; // prędkość wiatry
+        this.windDeg;   // kierunek wiatru
+        this.rain;      // wielkosc opadow dla ostatnich 1 - 3 h
+        this.clouds;    // 
+        this.snow;      // wielkosc opadow sniegu dla ostatnich 1 - 3 h 
+        this.id;        // id miasta ( do nastepnego wyszukania np. po id)
+        this.name;      // nazwa miasta
+        this.cod;       
     }
 
+    getweatherByCityName(cityName){
+        let url = `api.openweathermap.org/data/2.5/weather?q=${cityName}`;
+        fetch(url).then(r => r.json).then(data => console.log(data));
+    }
+
+    getweatherByCoordinates(lat,lon){
+        let url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}`;
+        fetch(url).then(r => r.json).then(data => console.log(data));
+    }
 
     query() {
         // TODO wykonanie zapytania do API i przypisania odpowiedzi do atrybutów
+        // Na razie testowo w
+        this.getweatherByCityName('London');
 
     }
 }
