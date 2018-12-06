c46773f9da9d5578dfa523060d8c0fbc71c9f89f
Wojciech Opulski <wojtekopulski@gmail.com>
2018-12-06 13:44:44 +0100

    testowe zapytania

--- src/weather.js
+++ src/weather.js
@@ -17,20 +17,34 @@ class Weather {
         this.cod;       
     }
 
-    getweatherByCityName(cityName){
-        let url = `api.openweathermap.org/data/2.5/weather?q=${cityName}`;
+    getWeatherByCityName(cityName){
+        let url = `https://www.api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=86677a0c14bfe5ca97291ba315d620e6`;
         fetch(url).then(r => r.json).then(data => console.log(data));
     }
 
+    getWeatherByCityNameAjax(){
+        //przykładowe zamyptanie porownawcze z AJAXA dla London,uk    ...nie działa:(
+        $.ajax({
+            method: 'GET',
+            url: `https://www.api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=86677a0c14bfe5ca97291ba315d620e6`,
+            dataType: 'json',
+        }).done(r => {
+            console.log(r);
+        }).fail( error => {
+            console.log(error);
+        });
+    }
+
     getweatherByCoordinates(lat,lon){
-        let url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}`;
+        let url = `https://www.api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}`;
         fetch(url).then(r => r.json).then(data => console.log(data));
     }
 
     query() {
         // TODO wykonanie zapytania do API i przypisania odpowiedzi do atrybutów
         // Na razie testowo w
-        this.getweatherByCityName('London');
+        // this.getWeatherByCityName('London');
+        this.getWeatherByCityNameAjax();
 
     }
 }
