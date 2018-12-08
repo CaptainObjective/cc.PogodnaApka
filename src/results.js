bd126f05f369620a53d23de70f10f4f14316f9b6
Kinga <k.u.pietrzak@gmail.com>
2018-12-08 17:19:05 +0100

    ble ble

--- src/results.js
+++ src/results.js
@@ -1,9 +1,28 @@
+
+let icon = document.getElementById("icon");
+let mainbar = document.getElementById("mainbar");
+let mainPressure = document.getElementById("mainPressure");
+let mainHumidity = document.getElementById("mainHumidity");
+let windSpeed = document.getElementById("windSpeed");
+let clouds = document.getElementById("clouds");
+let rain1h = document.getElementById("rain1h");
+let snow1h = document.getElementById("snow1h");
+
 class Results {
     constructor() {
-        console.log('Results loaded')
+        console.log('Results loadedb nevovnfenvenvevno')
     }
     refresh(weather) {
         // TODO: Podmiana tego co siedzi w html w oparciu o właściwosci otrzymanego obiektu
+
+        icon = `< i class = "${icon} icon" >< /i>`; // zmienna ma się ładnie podmieniać aby ładowała się stosowna ikona semantic
+        mainbar.innerHTML = weather.mainTemp && weather.cityName; //odświeżenie temp + nazwy miasta
+        mainPressure.innerHTML = weather.mainPressure + "hPa";
+        mainHumidity.innerHTML = weather.mainHumidity + "%";
+        windSpeed.innerHTML = weather.windSpeed + "km/h";
+        clouds.innerHTML = weather.clouds + "%";
+        rain1h.innerHTML = weather.rain1h + "%";
+        snow1h.innerHTML = weather.snow1h + "%";
     }
 }
 
