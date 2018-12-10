c922e2c9bb6ec1ab4e2231f2804ad5bd227bd28c
bartek <bartek.pazik@interia.pl>
2018-12-10 18:55:32 +0100

    update podpowiedzi

--- src/search.js
+++ src/search.js
@@ -20,68 +20,122 @@ class Search {
     refresh(weather) {
         // TODO: info dla użytkownika na podstawie pogody
         let tip;
+        let data = new Date();
+        let godzina = data.getHours();
 
-        console.log(weather.cityName, weather.mainTemp, weather.clouds);
-        if (weather.mainTemp > 24) {
-            if (weather.clouds < 20) {
-                tip = "Ciesz się upalnym i słonecznym dniem!";
-                return tip;
+        if(godzina > weather.sunrise && godzina < weather.sunset){
+            if (weather.mainTemp > 24) {
+                if (weather.clouds < 20) {
+                    tip = "Ciesz się upalnym i słonecznym dniem!";
+                    return tip;
+                }
+                else if(weather.clouds>=20 && weather.clouds <= 50) {
+                    tip = "To będzie upalny dzień. Spodziewaj się jednak zachmurzeń.";
+                    return tip;
+                }
+                else{
+                    tip="To będzie upalny dzień. Spodziewaj się jednak dużego zachmurzenia."
+                    return tip;
+                }
             }
-            else if(weather.clouds>=20 && weather.clouds <= 50) {
-                tip = "To będzie upalny dzień. Spodziewaj się jednak zachmurzeń.";
-                return tip;
+
+            else if (weather.mainTemp <= 24 && weather.mainTemp >= 17) {
+                if (weather.clouds < 20) {
+                    tip = "Ciesz się ciepłym i słonecznym dniem!";
+                    return tip;
+                }
+                else if(weather.clouds >=20 && weather.clouds <=50) {
+                    tip = "To będzie ciepły dzień. Spodziewaj się jednak zachmurzeń.";
+                    return tip;
+                }
+                else{
+                    tip = "To będzie ciepły dzień. Spodziewaj się jednak dużego zachmurzenia."
+                    return tip;
+                }
             }
-            else{
-                tip="To będzie upalny dzień. Spodziewaj się jednak dużego zachmurzenia."
-                return tip;
+
+            else if (weather.mainTemp < 17 && weather.mainTemp >= 10) {
+                if (weather.clouds < 20) {
+                    tip = "Przed tobą słoneczny i umiarkowanie ciepły dzień.";
+                    return tip;
+                }
+                else if(weather.clouds>=20 && weather.clouds<=50){
+                    tip = "Przed tobą umiarkowanie ciepły dzień. Spodziewaj się jednak zachmurzeń.";
+                    return tip;
+                }
+                else{
+                    tip = "Przed tobą umiarkowanie ciepły dzień. Spodziewaj się jednak dużego zachmurzenia."
+                    return tip;
+                }
+            }
+
+            else {
+                if (weather.clouds < 20) {
+                    tip = "Przed tobą chłodny dzień. Ubierz się ciepło.";
+                    return tip;
+                }
+                else if(weather.clouds>=20 && weather.clouds <=50) {
+                    tip = "To będzie chłodny i pochmurny dzień. Ubierz się ciepło.";
+                    return tip;
+                }
+                else{
+                    tip = "Przed tobą chłodny i bardzo pochmurny dzień. Ubierz się ciepło.";
+                    return tip;
+                }
             }
         }
 
-        else if (weather.mainTemp <= 24 && weather.mainTemp >= 17) {
-            if (weather.clouds < 20) {
-                tip = "Ciesz się ciepłym i słonecznym dniem!";
-                return tip;
+
+        else{
+            if (weather.mainTemp >= 17) {
+                if (weather.clouds < 20) {
+                    tip = "To będzie ciepła noc!";
+                    return tip;
+                }
+                else if(weather.clouds>=20 && weather.clouds <= 50) {
+                    tip = "To będzie ciepła i pochmurna noc!";
+                    return tip;
+                }
+                else{
+                    tip="To będzie ciepła i bardzo pochmurna noc!"
+                    return tip;
+                }
             }
-            else if(weather.clouds >=20 && weather.clouds <=50) {
-                tip = "To będzie ciepły dzień. Spodziewaj się jednak zachmurzeń.";
-                return tip;
+
+            else if (weather.mainTemp < 17 && weather.mainTemp >= 10) {
+                if (weather.clouds < 20) {
+                    tip = "To będzie umiarkowanie ciepła noc.";
+                    return tip;
+                }
+                else if(weather.clouds>=20 && weather.clouds<=50){
+                    tip = "To będzie umiarkowanie ciepła i pochmurna noc.";
+                    return tip;
+                }
+                else{
+                    tip = "To będzie umiarkowanie ciepła i bardzo pochmurna noc."
+                    return tip;
+                }
             }
-            else{
-                tip = "To będzie ciepły dzień. Spodziewaj się jednak dużego zachmurzenia."
-                return tip;
+
+            else {
+                if (weather.clouds < 20) {
+                    tip = "To będzie chłodna noc!";
+                    return tip;
+                }
+                else if(weather.clouds>=20 && weather.clouds <=50) {
+                    tip = "To będzie chłodna i pochmurna noc!";
+                    return tip;
+                }
+                else{
+                    tip = "To będzie chłodna i bardzo pochmurna noc!";
+                    return tip;
+                }
             }
+
         }
 
-        else if (weather.mainTemp < 17 && weather.mainTemp >= 10) {
-            if (weather.clouds < 20) {
-                tip = "Przed tobą słoneczny i umiarkowanie ciepły dzień.";
-                return tip;
-            }
-            else if(weather.clouds>=20 && weather.clouds<=50){
-                tip = "Przed tobą umiarkowanie ciepły dzień. Spodziewaj się jednak zachmurzeń.";
-                return tip;
-            }
-            else{
-                tip = "Przed tobą umiarkowanie ciepły dzień. Spodziewaj się jednak dużego zachmurzenia."
-                return tip;
-            }
-        }
-
-        else {
-            if (weather.clouds < 20) {
-                tip = "Przed tobą chłodny dzień. Ubierz się ciepło.";
-                return tip;
-            }
-            else if(weather.clouds>=20 && weather.clouds <=50) {
-                tip = "To będzie chłodny i pochmurny dzień. Ubierz się ciepło.";
-                return tip;
-            }
-            else{
-                tip = "Przed tobą chłodny i bardzo pochmurny dzień. Ubierz się ciepło.";
-                return tip;
-            }
-        }
     }
+
 
     showTip(tip) {
         // dodaje info dla uzytkownika do results
